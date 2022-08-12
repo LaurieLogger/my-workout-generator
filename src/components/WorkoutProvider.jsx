d691caa49266ae6765227afc9f8cf9840da95007
BumbleGoose <i.r.r.laurie@gmail.com>
2022-08-12 16:21:45 +0100

    add manual workout creation buttons with dropdown selection of reps and activities. NOTE: Perhaps extract to separate component

--- src/components/WorkoutProvider.jsx
+++ src/components/WorkoutProvider.jsx
@@ -1,6 +1,8 @@
 import BankAdder from "./BankAdder";
 import { useState } from "react";
 
+//REMEMBER TO ADD TIP GENERATOR ON WORKOUT GENERATION
+
 const WorkoutProvider = () => {
   const [activityBank, setActivityBank] = useState([
     { activity: "Pull-up", cals: 1, zone: "arms" },
@@ -10,8 +12,44 @@ const WorkoutProvider = () => {
     { activity: "Press-up", cals: 2, zone: "arms" },
     { activity: "Sit-up", cals: 2, zone: "core" },
   ]);
-  console.log(activityBank);
-  return <BankAdder setActivityBank={setActivityBank} />;
+
+  return (
+    <>
+      <BankAdder setActivityBank={setActivityBank} />
+
+      <span className="generator-grouping">
+        <form className="generator-grouping">
+          <label htmlFor="select-activity" className="input-label light-text">
+            Activity
+          </label>
+          <select id="select-activity" className="input-box">
+            {activityBank.map((activity) => {
+              return (
+                <option value={activity.activity}>{activity.activity}</option>
+              );
+            })}
+          </select>
+          <label
+            htmlFor="select-reps"
+            className="input-label light-text"
+            id="select-reps-label"
+          >
+            Reps:{" "}
+          </label>
+          <select id="select-reps" className="input-box">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+          <button className="light-text submit-button" id="add-activity-button">
+            Add +
+          </button>
+        </form>
+      </span>
+    </>
+  );
 };
 
 export default WorkoutProvider;
